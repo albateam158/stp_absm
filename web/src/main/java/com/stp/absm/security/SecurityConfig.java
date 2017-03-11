92c0323b1eeb2b92a098f15ef1b3227281024cda
shpark <shpark@kasset.co.kr>
2017-03-11 14:03:34 +0900

    20170311 스프링시큐리티수정

--- web/src/main/java/com/stp/absm/security/SecurityConfig.java
+++ web/src/main/java/com/stp/absm/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -24,58 +25,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 //	@Autowired
 //	private PasswordEncoder passwordEncoder;
-//    @Override
-//    public void configure(WebSecurity web) throws Exception {
-//        web
-//                .ignoring()
-//                .antMatchers("/**/*.js", "/**/**/*.js", "/**/*.css", "/font-awesome/**", "/**/**/*.css", "/**/*.png", "/**/*.gif", "/**/*.jpg", "/**/*.jpeg", "/**/*.ico");
-//    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/**/*.js", "/**/**/*.js", "/**/*.css", "/font-awesome/**", "/**/**/*.css", "/**/*.png", "/**/*.gif", "/**/*.jpg", "/**/*.jpeg", "/**/*.ico");
+    }
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-//		http
-//				.csrf().disable()
-//				.anonymous()
-//				.and()
-//				.formLogin()
-//				.loginPage("/login")
-//				.defaultSuccessUrl("/", true)
-//				.loginProcessingUrl("/auth")
-//				.failureHandler(new CustomLoginFailureHandler())
-//				//.failureUrl("/login")
-//			.and()
-//				.logout()
-//				.deleteCookies("remember-me")
-//				.logoutUrl("/logout")
-//				.logoutSuccessUrl("/login")
-//			.and()
-//				.rememberMe().and()
-//				.authorizeRequests()
-//				.antMatchers(
-//						"/auth/**",
-//						"/login",
-//						"/error",
-//						"/signup",
-//						"/assets/**",
-//						"/projectdownload/**",
-//						"/pages/**",
-//						"/admin/form",
-//						"/user/form_pre",
-//						"/user/form",
-//						"/emp/listV",
-//                        "/emp/phone_form",
-//						"/emp/list_div",
-//						"/emp/phoneV",
-//						"/emp/phone_div",
-//						"/inter_rule/listV",
-//						"/inter_rule/list_div",
-//						"/ruleFile2",
-//						"/ruleFile",
-//                        "/getByteImage1/**"
-//				).permitAll()
-//				.antMatchers("/**").hasRole("SUPERVISOR").and()
-//				.addFilterAfter(new AjaxSessionTimeoutFilter(), ExceptionTranslationFilter.class);
+		http
+				.csrf().disable()
+				.anonymous()
+ 				.and()
+				.addFilterAfter(new AjaxSessionTimeoutFilter(), ExceptionTranslationFilter.class);
+		;
 	}
 
 	@Bean
