0af7473389e943fb5c5ce6c4f464c87b5878ae07
shpark <shpark@kasset.co.kr>
2017-03-27 13:41:52 +0900

    20170327 webconfig수정

--- web/src/main/java/com/stp/absm/controller/WebMvcConfig.java
+++ web/src/main/java/com/stp/absm/controller/WebMvcConfig.java
@@ -42,10 +42,10 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Autowired
     protected DurationFromNow durationFromNow;
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry){
-        registry.addResourceHandler("/file/**").addResourceLocations("file:///"+fileLocation);
-    }
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry){
+//        registry.addResourceHandler("/file/**").addResourceLocations("file:///"+fileLocation);
+//    }
 
     @Bean
     public Map<String, TemplateModel> freemarkerLayoutDirectives() {
@@ -80,6 +80,15 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         viewResolver.setSuffix(".ftl");
         viewResolver.setContentType("text/html; charset=utf-8");
         return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        /**
+         * 이부분의 addResourceLocations 경로를 설정에서 읽어오는 데이터 바인딩 하는게
+         * 문제가 있어서 설정과 동일한 경로로 직접 하드코딩 입력
+         */
+        registry.addResourceHandler("/file/**").addResourceLocations("file:///C:\\workspace\\absm\\upload\\");
     }
 
 
